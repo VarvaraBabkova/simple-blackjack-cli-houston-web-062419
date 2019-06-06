91789ab65ae397c9d368d8170bb9a6994d088f57
Varvara Babkova <varvara.munday@gmail.com>
2019-06-06 18:08:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,8 +8,9 @@ def deal_card
   return 1 + rand(11)
 end
 
-def display_card_total
+def display_card_total (card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
